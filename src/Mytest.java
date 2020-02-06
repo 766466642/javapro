13201682daf9fea2a3bbf1ab6541a272d361d931
766466642 <766466642@qq.com>
2020-02-06 16:38:42 +0800

    test

---
+++ src/Mytest.java
@@ -0,0 +1,10 @@
+/**
+ * Created by HASEE on 2020/2/6.
+ */
+public class Mytest {
+    public static void main(String[] args) {
+        int a=10;
+        int b=10;
+        System.out.println(a+b);
+    }
+}
