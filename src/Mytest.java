d7cb3c4772b1d6777e7037606ca0b7b3d5b5f9cd
766466642 <766466642@qq.com>
2020-02-10 22:34:36 +0800

    test

--- src/Mytest.java
+++ src/Mytest.java
@@ -1,10 +1,17 @@
+package com.neuedu.Test;
 /**
- * Created by HASEE on 2020/2/6.
+ * Created by 13930 on 2020/2/5.
  */
 public class Mytest {
     public static void main(String[] args) {
-        int a=10;
-        int b=10;
-        System.out.println(a+b);
+        Circle circle = new Circle(2,"black");
+        Rectangle rectangle = new Rectangle(3,5,"red");
+        circle.getArea();
+        circle.getPer();
+        circle.showAll();
+
+        rectangle.getArea();
+        rectangle.getPer();
+        rectangle.showAll();
     }
 }
